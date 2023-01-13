0daf4218e592f30a3cf564f8c838969a27d31915
Яковлев Андрей Михайлович <andrei.yakauleu@softclub.by>
2023-01-13 14:37:58 +0300

    Alpha version

--- src/main/java/sc/dev/cd/SecurityConfig.java
+++ src/main/java/sc/dev/cd/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        corsConfiguration.setAllowedOrigins(List.of("http://192.168.249.62:8088"));
+        corsConfiguration.setAllowedOrigins(List.of("http://192.168.244.112:8088"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setExposedHeaders(List.of("Authorization"));
